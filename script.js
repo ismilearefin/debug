eb3942067916f0038bf3abbe66e52a2aa603eaf1
ismilearefin <arefinismile@gmail.com>
2022-09-14 20:57:22 +0600

    check data

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let questionText = "";
 fetch("./texts.json")
   .then((res) => res.json())
   .then((data) => {
+    // console.log(data)
     questionText = data[Math.floor(Math.random() * data.length)];
     question.innerHTML = questionText;
   });
